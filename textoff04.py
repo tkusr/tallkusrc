b120e71d70532dc20dd0c04cc41e5029df4f6f2c
tallku <1621680732@qq.com>
2020-03-07 00:40:49 +0800

    crack for office

--- textoff04.py
+++ textoff04.py
@@ -9,15 +9,7 @@ def progressbar(nowprogress,toyal):    #nowprogress现在的进度数   toyal#�
     get_pro=int(50-get_progress)#显示多少-
     percent=(nowprogress+1)*(100/toyal)
     print("\r"+"["+">"*get_progress+"-"*get_pro+']'+"%.2f" % percent + "%",end="")
-# Use password
-# file.load_key(password="12345678")
 
-# Use private key
-# file.load_key(private_key=open("priv.pem", "rb"))
-# Use intermediate key (secretKey)
-# file.load_key(secret_key=binascii.unhexlify("AE8C36E68B4BB9EA46E5544A5FDB6693875B2FDE1507CBC65C8BCF99E25C2562"))
-list = ["123456","134498","4654165","12345678"]
-# password = "12345678"
 passFile=open(r'3_100/pass0.txt',encoding="utf-8")
 zpw=passFile.readlines()
 for line in zpw:
